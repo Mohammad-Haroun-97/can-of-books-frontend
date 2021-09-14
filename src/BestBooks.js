4d193bcfb7d23d9b371efc7d2e6c700fac50dd2f
Mohammad-Haroun-97 <harounmohammad@yahoo.com>
2021-09-14 17:10:42 +0300

    finish modulization and the render condition

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
-import bookItem from './bookItem';
+import BookItem from '../src/BookItem';
 import { withAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 import Carousel from 'react-bootstrap/Carousel'
@@ -14,8 +14,9 @@ class MyFavoriteBooks extends React.Component {
   
   constructor(props){
     super(props);
-    this.state = {
-      booksArr : []
+
+    this.state ={
+      booksArr : [],
     }
   }
 
@@ -28,6 +29,7 @@ class MyFavoriteBooks extends React.Component {
       this.setState({
         booksArr:result.data
       })
+      // console.log(result.data);
     })
     .catch (err =>{
       console.log('error');
@@ -41,52 +43,21 @@ class MyFavoriteBooks extends React.Component {
 
 
     return(
-      <Jumbotron>
+      <>
+      
         <h1>My Favorite Books</h1>
         
 
-        {this.state.booksArr.map(item=>{
-          return(
-            <>
-
+       
+        
             
-
-               {/* <p>{item.title}</p>
-                <p>{item.description}</p>
-                <p>{item.status}</p>
-                <p>{item.email}</p> */}
-
-
-                <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={item.status}
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>{item.title}</h3>
-      <p>{item.description}</p>
-      <p>{item.email}</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-
-  </Carousel>
-  {/* <Carousel.Item></Carousel.Item> */}
-
-
-
-
-            {/* // title =  <p>{item.title}</p>
-            // description = {item.description}
-            // status = {item.status}
-            // email = {item.email} */}
-
-            </>
-          )
-        })
-       }
-      </Jumbotron>
+          {this.state.booksArr.length && (<BookItem booksArr = {this.state.booksArr}></BookItem>)}
+            
+          
+        
+       
+     
+      </>
     )
   }
 }
